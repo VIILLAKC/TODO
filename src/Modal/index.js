7dd8de02bc7698f2ee0d57addf090e9615d68d04
Israel Villa <israelvilla112@gmail.com>
2023-12-15 12:19:45 -0600

    bug in modal fixed

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -1,19 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { TodoContext } from "../TodoContext";
 import "./Modal.css"
 
 function Modal({ children }){
 
-    const { openModal } = React.useContext(TodoContext);
-
     return ReactDOM.createPortal(
-    <div className={`modal ${openModal ? 'modal-active' : ''}`}>
+      <div className="modal-container">
         <label htmlFor="close-modal"  className="modal__bg"></label>
         <div className="modal-body">
           { children }
         </div>
-    </div>,
+      </div>,
     document.getElementById('modal')
     );
 }
